5888d6dbbe019ba4933c2117c5f44479430c5dea
DESKTOP-M90QUJH\yegan <yeganeh.b1379@gmail.com>
2022-08-31 17:48:07 +0430

    animation added

--- main.js
+++ main.js
@@ -21,3 +21,14 @@ const navClose = () => {
 navIconEl.addEventListener("click", navOpen);
 navCloseEl.addEventListener("click", navClose);
 navBgOverlay.addEventListener("click", navClose);
+
+// aos
+AOS.init({
+  offset: 200,
+  delay: 100,
+  duration: 400,
+  easing: "ease",
+  once: false,
+  mirror: false,
+  anchorPlacement: "top-bottom",
+});
